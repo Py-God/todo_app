f9a04f131639b961356f8310d7a0686e97817033
Boluwatife <boluwatifelekeoduoye@gmail.com>
2024-07-01 11:55:58 +0100

    first commit for adding font

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import (
+    Flask,
+    render_template,
+    session
+    )
+from flask_session import Session
+
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+
+    return render_template("login.html")
+
+
+@app.route("/")
+def index():
+    return render_template("layout.html")
