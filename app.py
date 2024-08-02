5dd68e4fae443eda6557253d268286ac66b00ee1
Boluwatife <boluwatifelekeoduoye@gmail.com>
2024-08-02 12:41:50 +0100

    final changes to the web app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from flask import (
     abort,
     Flask,
@@ -11,15 +11,16 @@ from flask_migrate import Migrate
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from helper import login_required, date
-import logging
 import re
 from sqlalchemy import desc, func, MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# declare model base
 class Base(DeclarativeBase):
     pass
 
+# some sqlalchemy nameing convention
 naming_convention = {
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -38,8 +39,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///taskhero.db"
 # initialize the app with the extension
 db.init_app(app)
 
+# flask migrate for shell
 migrate = Migrate(app, db, render_as_batch=True)
 
+# data base model for registered users
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True, nullable=False)
@@ -50,7 +53,7 @@ class User(db.Model):
     def __repr__(self):
         return f"<User {self.id}>"
     
-
+# data base model for registered users tasks
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=True, nullable=False)
@@ -61,7 +64,7 @@ class Task(db.Model):
     def __repr__(self):
         return f"<Task {self.id}>"
     
-    
+# create all db tables
 with app.app_context():
     db.create_all()
 
@@ -83,10 +86,13 @@ def after_request(response):
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+
+        # get register field values
         email = request.form.get("email")
         password = request.form.get("password")
         confirm_password = request.form.get("confirm_password")
 
+        # validate fields
         if not email or not password or not confirm_password:
             abort(400, "Missing Fields")
 
@@ -100,12 +106,16 @@ def register():
         if password != confirm_password:
             abort(400, "Passwords do not match")
 
+        # generate password hash
         password_hash = generate_password_hash(password, method="scrypt", salt_length=16)
 
+        # get last user id in database
         last_user_id = get_last_user_id()
 
+        # prepopulate username field with a custom field
         username = f"Productive User {last_user_id + 1}"
 
+        # add user to database
         new_user = User(username=username, email=email, password_hash=password_hash)
         db.session.add(new_user)
         db.session.commit()
@@ -122,11 +132,13 @@ def login():
     # clear session id: forget any previous user
     session.clear()
 
-    # TODO write login logic
     if request.method == "POST":
+
+        # get user login details
         email = request.form.get("email")
         password = request.form.get("password")
 
+        # validate login details
         if not email or not password:
             abort(400, "Missing Fields")
 
@@ -159,10 +171,15 @@ def logout():
 @app.route("/")
 @login_required
 def index():    
-    tasks = db.session.execute(db.select(Task).order_by(desc(Task.id))).scalars()
+    # get user specific tasks from database
+    tasks = db.session.execute(
+        db.select(Task).filter_by(user_id=session["user_id"]).order_by(desc(Task.id))
+        ).scalars()
 
+    # get current user details from database
     user = User.query.filter_by(id=session["user_id"]).first()
 
+    # create custom dictionary to enable easy display of tasks in particular format
     task_dict = {}
 
     for task in tasks:
@@ -178,14 +195,19 @@ def index():
 @login_required
 def add_task():
     if request.method == "POST":
+
+        # get inputted task name from form
         task_name = request.form.get("task_name")
 
+        # validate if task name exists
         if not task_name:
             abort(400, "Invalid Task Name")
 
+        # get current date and time to attribute to task
         time = datetime.strftime(datetime.now(), "%I:%M %p")
         date = datetime.strftime(datetime.now(), "%Y-%m-%d")
 
+        # add task to database
         new_task = Task(name=task_name, time=time, date=date, user_id=session["user_id"])
         db.session.add(new_task)
         db.session.commit()
@@ -199,9 +221,13 @@ def add_task():
 @app.route("/edit_task/<int:id>", methods=["GET", "POST"])
 @login_required
 def edit_task(id):
-    task = db.one_or_404(db.select(Task).filter_by(id=id))
+
+    # get soon to be edited task from database
+    task = db.one_or_404(db.select(Task).filter_by(id=id, user_id=session["user_id"]))
 
     if request.method == "POST":
+
+        # change the name of the task
         task.name = request.form.get("task_name")
         db.session.commit()
 
@@ -214,9 +240,14 @@ def edit_task(id):
 @login_required
 def delete_task():
     if request.method == "POST":
-        id = request.form.get("id")
-        task = db.one_or_404(db.select(Task).filter_by(id=id))
 
+        # get soon to be deleted task from database
+        id = request.form.get("id")
+        task = db.one_or_404(
+            db.select(Task).filter_by(id=id, user_id=session["user_id"])
+            )
+
+        # delete task
         db.session.delete(task)
         db.session.commit()
 
@@ -228,15 +259,21 @@ def delete_task():
 @app.route("/change_username", methods=["GET", "POST"])
 @login_required
 def change_username():
+
+    # get current user details from database
     user = db.one_or_404(db.select(User).filter_by(id=session["user_id"]))
 
     if request.method == "POST":
+
+        # get new username input from form
         new_username = request.form.get("new_username")
 
+        # validate if new username already exists in database
         already_exists = User.query.filter_by(username=new_username).first()
         if already_exists and already_exists.id != session["user_id"]:
             abort(400, "This username is not available")
 
+        # update username
         user.username = request.form.get("new_username")
         db.session.commit()
 
@@ -245,6 +282,7 @@ def change_username():
         abort(400, "Change Username does not have a get method")
 
 
+# get last user id from database to be able to create new user username
 def get_last_user_id():
     last_user_id = db.session.query(func.max(User.id)).scalar()
     if last_user_id is None:
@@ -252,7 +290,7 @@ def get_last_user_id():
     else:
         return last_user_id
     
-
+# be able to work with flask shell
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User}
--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from flask import redirect, session
 from functools import wraps
 
+# prevents foreign user from entering page without loggin in
 def login_required(f):
 
     @wraps(f)
@@ -13,6 +14,7 @@ def login_required(f):
     return decorated_function
 
 
+# return date in a particular format
 def date(input_date):
     formatted_date = datetime.strptime(input_date, "%Y-%m-%d").date()
     now = datetime.now().date()
